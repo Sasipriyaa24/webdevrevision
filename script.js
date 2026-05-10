ac42814ac84e03021daa6b5d8eb437f91c7f35dd
Sasi Priyaa <sasipriyaa4424@gmail.com>
2026-05-10 12:51:10 +0530

    revision of js

---
+++ script.js
@@ -0,0 +1,27 @@
+/*let obj={
+  name:"sasi",
+  age:10
+}
+console.log(obj.name);
+
+function sasi() {
+  console.log("priyaa");
+}
+sasi();
+
+console.log(true==1);
+*/
+const a=20;
+const res=a>=20?"eligible":"no";
+console.log(res);
+
+const obj={
+  name:'sasi'
+};
+
+console.log(obj?.contact);
+
+
+for(let i=0;i<=5;i++){
+  console.log(i);
+};
