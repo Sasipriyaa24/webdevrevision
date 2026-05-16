b903cc8f17ecec441333204319262c13701641af
Sasi Priyaa <sasipriyaa4424@gmail.com>
2026-05-16 15:02:08 +0530

    get

--- node-app/server.js
+++ node-app/server.js
@@ -1,7 +1,11 @@
 const express=require('express');
 const app = express();
-app.get('/',(req,res)=>{
-  res.send("API is Working");
+app.get('/courses',(req,res)=>{
+  const courses=[
+    {id:24,name:'Sasi',tile:'js'},
+    {id:17,name:'keerthi',title:'da'}
+  ];
+  res.json(courses);
 });
 app.listen(3000,() => {
   console.log("running");
