721df3318d3bf19131a4ae5d7e5e5977020209b4
Sasi Priyaa <sasipriyaa4424@gmail.com>
2026-06-02 11:00:56 +0530

    Rendered the react element

--- first_react_project/src/App.jsx
+++ first_react_project/src/App.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-const Welcome=(props)=>{
-  return <div>{props.children}</div>
-}
 function App(){
   return(
-    <Welcome>
       <h1>Hello World</h1>
-    </Welcome>
   );
 };
 export default App;
