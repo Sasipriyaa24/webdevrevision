89f9b9c10edba9c1b9dabc069dda4228db8eff2b
Sasi Priyaa <sasipriyaa4424@gmail.com>
2026-06-08 08:48:41 +0530

    react forms

--- first_react_project/src/App.jsx
+++ first_react_project/src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
-function App(){
-  return(
-      <h1>Hello World</h1>
-  );
-};
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+
+    function onInputChange(event) {
+        console.log(event.target.value);
+    }
+
+    return (
+        <div>
+            <form>
+                <label>Enter text</label>
+                <input 
+                    type="text"
+                    onChange={onInputChange}
+                />
+            </form>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
 export default App;
